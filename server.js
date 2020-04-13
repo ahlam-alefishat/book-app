cdc7da71c86737f9dfa046287e4af7091622f24d
ahlamalefishat96 <ahlam.alefishat@gmail.com>
2020-04-14 01:52:04 +0300

    didnt finish yet

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const superagent=require('superagent');
+const pg= require('pg');
 
 
 const PORT = process.env.PORT || 3000;
+const client = new pg.Client(process.env.DATABASE_URL);
+
 
 //Specify a directory for a statis resourses 
 app.use(express.static('./public'));
@@ -25,7 +28,6 @@ app.get('/searches/new', (req, res) => {
 });
 
 app.post('/searches', (req, res) => {
- 
   let url; 
   if (req.body.radioType === 'title') {
       url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.search}&intitle:${req.body.search}`;
@@ -47,20 +49,30 @@ app.post('/searches', (req, res) => {
 });
 
 function Book(book){
-  this.title = book.volumeInfo.title;
-  this.author = book.volumeInfo.authors[0];
-  this.img = book.volumeInfo.imageLinks.smallThumbnail;
-  this.description = book.volumeInfo.description;
+const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = book.title ? book.title : 'Title not available';
+  this.author = book.authors ? book.authors[0] : 'Author not available';
+  this.isbn = book.industryIdentifiers ? book.industryIdentifiers[0].identifier : 'ISBN not available';
+  this.image_url = book.imageLinks ? book.imageLinks.thumbnail : placeholderImage;
+  this.description = book.description ? book.description : 'No description';
+  this.bookshelf = book.categories ? book.categories[0] : 'Uncategorized';
+
 }
+
 
 function errorHandler (error,req,res){
   res.status(500).send(error);
 }
 
+
 app.get('*', (req, res) => {
   res.status(404).send('NO SUCH ROUTE!');
-})
-
-app.listen(PORT, () => {
-  console.log(`Listening on PORT${PORT}`);
 });
+
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on PORT${PORT}`);
+    });
+  });
